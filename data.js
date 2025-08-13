c01c76171b1d112644aa3fd0847af348643a784f
Arya1015 <lakarearya1015@gmail.com>
2025-08-13 21:36:23 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,6 @@
+export const items = [
+  { id: 101, title: "Echo Dot (4th Gen)", price: 4499, rating: 4.4, img: "https://picsum.photos/seed/101/600/400" },
+  { id: 102, title: "Kindle Basic",       price: 7999, rating: 4.6, img: "https://picsum.photos/seed/102/600/400" },
+  { id: 103, title: "Fire TV Stick",      price: 2999, rating: 4.5, img: "https://picsum.photos/seed/103/600/400" },
+  { id: 104, title: "Microwave Oven",     price: 9299, rating: 4.2, img: "https://picsum.photos/seed/104/600/400" }
+];
